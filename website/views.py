14566a28926f5082855c655ac5cff3f40115e9b5
mcfreak12 <jacobfrank714@gmail.com>
2023-02-08 12:53:11 -0600

    adding flask starter code

---
+++ website/views.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template, request
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET'])
+def home():
+    return render_template("main.html")
