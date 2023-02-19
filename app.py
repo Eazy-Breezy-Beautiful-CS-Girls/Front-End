022bafa65efb247bc52db19b5b4bff2d6875b13a
mcfreak12 <jmft87@mail.umkc.edu>
2023-02-19 15:39:06 -0600

    rearanging items and index works with flask now

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from website import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+app.config['SECRET_KEY']='sh its a secret'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def index():
+    return render_template('index.html')
