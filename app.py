9be1e70c826762d30d1e69fc7e67b983b7136bc3
mcfreak12 <jacobfrank714@gmail.com>
2023-02-22 12:30:43 -0600

    most page links work now

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY']='sh its a secret'
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/about.html', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/causes.html', methods=['GET'])
+def causes():
+    return render_template('causes.html')
+
+@app.route('/blog.html', methods=['GET','POST'])
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact.html', methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+@app.route('/single.html', methods=['GET'])
+def single():
+    return render_template('single.html')
