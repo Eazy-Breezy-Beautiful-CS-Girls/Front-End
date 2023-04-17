1e106c3b13e01d56cd401830bd0921453320fdcb
mcfreak12 <jacobfrank714@gmail.com>
2023-04-17 11:54:07 -0500

    probably won't use stripe

--- FrontEnd/__init__.py
+++ FrontEnd/__init__.py
@@ -12,8 +12,8 @@ def create_app():
     
     app.register_blueprint(auth.bp)
     app.register_blueprint(fund.bp)
-    import stripe
-    stripe.api_key = "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"
+    # import stripe
+    # stripe.api_key = "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"
 
     app.add_url_rule("/", endpoint="index")
     
