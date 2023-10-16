70d24762df0d231d60cef64aae3a7f041da3e792
mcfreak12 <jmft87@mail.umkc.edu>
2023-10-15 20:01:07 -0500

    updating database endpoint

--- FrontEnd/database.py
+++ FrontEnd/database.py
@@ -5,14 +5,15 @@ from flask import g
 def get_db():
     if "db" not in g:
         conn = pymysql.connect(
-        host= 'database-1.ctvfyumkix2j.us-east-2.rds.amazonaws.com', 
-        port = 3306,
-        user = 'admin', 
-        password = 'ezbreezy',
-        db = 'mydb',
+            host="database-1.ctvfyumkix2j.us-east-2.rds.amazonaws.com",
+            port=3306,
+            user="admin",
+            password="ezbreezy",
+            db="mydb",
         )
-        g.db=conn
+        g.db = conn
     return g.db
+
 
 def close_db(e=None):
     """If this request connected to the database, close the
@@ -23,10 +24,9 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+
 def init_app(app):
     """Register database functions with the Flask app. This is called by
     the application factory.
     """
     app.teardown_appcontext(close_db)
-
-    
